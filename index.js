7518a6dd56907a67cdc28eda0a0b7ea6dc90f20b
GuilleBlanco08 <guille.blanco.diaz@gmail.com>
2025-02-25 10:19:30 +0100

    Update

--- index.js
+++ index.js
@@ -1,9 +1,16 @@
 const express = require("express");
+const cool = require("cool-ascii-faces");
 const app = express();
 const PORT = 16078;
 
-app.get("/",(request, response)=>{
+app.use("/",express.static("./public"));
+
+app.get("/hello",(request, response)=>{
     response.send("Hello from the server!");
+});
+
+app.get("/cool",(request, response)=>{
+    response.send(cool());
 });
 
 app.listen(PORT,()=>{
